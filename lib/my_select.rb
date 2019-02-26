b427ff5cde70f58f02d5b62d9ddcb08e98f73211
Samuel Guo <guosamuel1114@gmail.com>
2019-02-26 21:45:13 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,6 +3,9 @@ def my_select(collection)
   new_collection = []
  
   while counter < collection.length
-  
+    new_collection.push(yield collection[counter])
+    counter = counter + 1
   end
+  
+  new_collection
 end
